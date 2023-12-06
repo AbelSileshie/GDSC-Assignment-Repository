27ac926bc28d6be107f709c1f4aaccd70a736847
Abel S <150824348+AbelSileshie@users.noreply.github.com>
2023-12-06 13:32:33 +0300

    Introduction to JS

---
+++ Introduction to JS/BMI_Calculater/Script.js
@@ -0,0 +1,7 @@
+function BMIcalac(){
+ let A = prompt("enter your Weight")
+ let B = prompt("Enter your Height")
+ const BMI= A/(B*B);
+ prompt("Your BMI is: "+BMI)
+}
+BMIcalac()
---
+++ Introduction to JS/JS_todo_App/script.js
@@ -0,0 +1,27 @@
+const todoList = document.getElementById("todoList");
+const newTodoInput = document.getElementById("newToDoInput");
+const addTodoBtn = document.getElementById("addTodoBtn");
+
+addTodoBtn.addEventListener("click", () => {
+  const newTodoText = newTodoInput.value;
+
+  if (newTodoText !== "") {
+    const newTodoItem = document.createElement("ul");
+    newTodoItem.innerText = newTodoText;
+    const strikebox = document.createElement("input");
+    strikebox.type = "checkbox";
+
+    strikebox.classList.add("strikebox");
+    strikebox.addEventListener("change", function () {
+      if (this.checked) {
+        newTodoItem.style.textDecoration = "line-through";
+      } else {
+        newTodoItem.style.textDecoration = "none";
+      }
+    });
+
+    newTodoItem.appendChild(strikebox);
+    todoList.appendChild(newTodoItem);
+    newTodoInput.value = "";
+  }
+});
